c9a4bf0622d02cb694f926d4dc8c4508d7aa1fdc
Jasonzss <2418972236@qq.com>
2024-01-09 17:54:54 +0800

    无关修改

---
+++ tics-common/tics-common-mybatis/src/main/java/com/jason/tics/common/dao/config/MybatisConfig.java
@@ -0,0 +1,16 @@
+package com.jason.tics.common.dao.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * @author Jason
+ */
+@Configuration
+public class MybatisConfig {
+
+}
