7298cdd26e4705bde260f7c75b7bf813e800ffa9
Jasonzss <2418972236@qq.com>
2024-02-07 17:34:46 +0800

    修改文件夹名

---
+++ tics-common/tics-common-mybatis/src/main/java/com/jason/tics/common/mybatis/config/MybatisConfig.java
@@ -0,0 +1,11 @@
+package com.jason.tics.common.mybatis.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Jason
+ */
+@Configuration
+public class MybatisConfig {
+
+}
