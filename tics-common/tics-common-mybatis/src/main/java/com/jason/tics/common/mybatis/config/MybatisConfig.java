35c4cec1a30f6bdda35a73fb2411ea61d56111fa
Jasonzss <2418972236@qq.com>
2024-02-19 15:21:59 +0800

    暂时移除无用类

--- tics-common/tics-common-mybatis/src/main/java/com/jason/tics/common/mybatis/config/MybatisConfig.java
+++ tics-common/tics-common-mybatis/src/main/java/com/jason/tics/common/mybatis/config/MybatisConfig.java
@@ -1,11 +1,8 @@
 package com.jason.tics.common.mybatis.config;
 
-import org.springframework.context.annotation.Configuration;
-
 /**
  * @author Jason
  */
-@Configuration
 public class MybatisConfig {
 
 }
